760fd184428be8e24dd61264ca158ddcbb7611fa
Jake Love <rockymountainhigh1943@gmail.com>
2011-05-01 00:23:32 -0700

    push again

--- application.rb
+++ application.rb
@@ -1,13 +1,18 @@
 require "sinatra"
 
+helpers do
+    include Rack::Utils
+    alias_method :h, :escape_html
+end
+
 ### Home
 get '/' do
 	erb :main
 end
 
 ### Login Attempt
-post '/' do
-	erb :main
+post '/account' do
+	erb :account, :locals => {:name => params[:username]}
 end
 
 ########## Admin ##########
