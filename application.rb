2640a75c1db1b915c9efbe3e2083269868e88d72
Jake Love <rockymountainhigh1943@gmail.com>
2011-05-02 00:14:26 -0700

    push switch.erb

--- application.rb
+++ application.rb
@@ -5,16 +5,24 @@ helpers do
     alias_method :h, :escape_html
 end
 
+### Home
 get '/' do
 	erb :main
 end
 
+### Dashboard
 post '/account' do
 	erb :account, :locals => {:name => params[:username]}
 end
 
+### View User Events
 get '/account/view/' do
 	erb :view
+end
+
+### Request Switch
+get '/account/switch/' do
+	erb :switch
 end
 
 ########## Admin ##########
