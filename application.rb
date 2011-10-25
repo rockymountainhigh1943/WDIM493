28fa4afbb09dce818fb9325d8a9c6e6f0ac76ed8
Jake Love <rockymountainhigh1943@gmail.com>
2011-10-24 21:51:08 -0700

    A whole bunch of changes

--- application.rb
+++ application.rb
@@ -53,7 +53,24 @@ end
 ### Signup
 get '/account/signup/?' do
 	redirect '/' if !@user
+	@ranges = EventRange.all(:active => 1)
 	erb :signup, :locals => { :active => 'signup' }
+end
+
+### Signup Monthly View
+get '/account/signup/monthly/:range' do |range|
+	redirect '/' if !@user
+	@month = EventRange.first(:erid => params['range'])
+	@events = Event.all(:event_range => params['range'])
+	@userStatus = UserEvent.all(:owner_id => @user.id)
+	erb :monthly, :locals => { :active => 'signup' }
+end
+
+get '/account/signup/event/:event' do |event|
+	redirect '/' if !@user
+	splitArgs = params['event'].split('-')
+	signup = UserEvent.first_or_create(:ueid => nil , :owner_id => splitArgs[0], :event_id => splitArgs[1], :status => 1 )
+	redirect '/account/signup/monthly/'+splitArgs[2]
 end
 
 ### View User Events
@@ -115,6 +132,31 @@ get '/admin/?' do
 	erb :admin, :locals => { :active => 'admin' }
 end
 
+### Add User
+
+get '/admin/users/?' do
+	redirect '/' if !@user.is_admin
+	@users = User.all
+	erb :users, :locals => { :active => 'admin' }
+end
+
+post '/admin/users/add' do
+	redirect '/' if !@user.is_admin
+	newUser = User.first_or_create( :id => nil, :first_name => params['first'], :last_name => params['last'], :email => params['email'], :username => params['username'], :password => params['password'], :is_admin => params['admin'] )
+	redirect '/admin/users?status=added'
+end
+
+### Delete User
+
+get '/admin/users/delete/:delete' do |delete|
+	redirect '/' if !@user.is_admin
+	deleteUser = User.first(:id => params['delete'])
+	if deleteUser
+		deleteUser.destroy
+	end
+	redirect '/admin/users?status=deleted'
+end
+
 ### Add Event
 get '/admin/add/event/r/:range/?' do |range|
 	redirect '/' if !@user.is_admin
@@ -128,7 +170,23 @@ post '/admin/add/event/r' do
 	redirect '/admin/schedule/monthly/'+params['event_range_id']
 end
 
+### Edit Event
+get '/admin/edit/event/:event' do |event|
+	redirect '/' if !@user.is_admin
+	splitArgs = params['event'].split('-')
+	@event = Event.first(:eid => splitArgs[0])
+	@eventRange = EventRange.first(:erid => splitArgs[1])
+	erb :admin_edit_event, :locals => { :active => 'admin' }
+end
 
+post '/admin/edit/event' do
+	redirect '/' if !@user.is_admin
+	updateEvent = Event.first(:eid => params['event_id'])
+	if updateEvent
+		updateEvent.update(:event_name => params['event_name'], :event_day => params['event_day'], :event_day_name => params['event_day_name'], :event_time => params['event_time'], :type => params['type'], :location => params['location'], :need => params['need'], :notes => params['notes'])
+	end
+	redirect '/admin/schedule/monthly/'+params['event_range_id']
+end
 
 ### Delete Event
 get '/admin/delete/event/:delete' do |event|
@@ -140,8 +198,6 @@ get '/admin/delete/event/:delete' do |event|
 	end
 	redirect '/admin/schedule/monthly/'+splitArgs[1]
 end
-
-
 
 ### Schedule
 get '/admin/schedule/?' do
@@ -221,7 +277,39 @@ get '/admin/schedule/monthly/:range' do |range|
 	redirect '/' if !@user.is_admin
 	@month = EventRange.first(:erid => params['range'])
 	@events = Event.all(:event_range => params['range'])
-	#@types = EventType.get(3)
-	#@locations = EventLocation.first(:elid => @events.location)
 	erb :admin_monthly, :locals => { :active => 'admin' }
+end
+
+### Event View
+get '/admin/schedule/event/:event' do |event|
+	redirect '/' if !@user.is_admin
+	splitArgs = params['event'].split('-')
+	@range = EventRange.first(:erid => splitArgs[1])
+	@event = Event.first(:eid => splitArgs[0])
+	type_ = @event.type
+	@type = EventType.first(:etid => type_)
+	erb :admin_event, :locals => { :active => 'admin' }
+end
+
+### Add Positions
+get '/admin/positions' do
+	redirect '/' if !@user.is_admin
+	@position = Position.all
+	erb :positions, :locals => { :active => 'admin' }
+end
+
+post '/admin/positions/add' do
+	redirect '/' if !@user.is_admin
+	newPosition = Position.first_or_create(:pid => nil, :name => params['name'])
+	redirect '/admin/positions?status=added'
+end
+
+### Delete Position
+get '/admin/position/delete/:position' do |position|
+	redirect '/' if !@user.is_admin
+	deletePosition = Position.first(:pid => params['position'])
+	if deletePosition
+		deletePosition.destroy
+	end
+	redirect '/admin/positions?status=deleted'
 end
